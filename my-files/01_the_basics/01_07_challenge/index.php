06f7f9f8750a23d2579230af2250c58284db1443
J-Ahmed-Gaafar <ahmed.fhesham@jumia.com>
2024-12-29 14:01:27 +0200

    Challenge: Display basic information on an HTML Page

---
+++ my-files/01_the_basics/01_07_challenge/index.php
@@ -0,0 +1,9 @@
+<?php 
+$name= "Teresa"; // Heading 1
+$city = "NYC";
+$movie = "Star Wars"; //Italics
+$friend = "George"; //Bold
+$candy = "Sour Patch Kids";
+
+echo 'In ' . $city . ', A girl named <h1>' . $name . '</h1> went to the movies with her friend <strong>' . $friend . "</str=>.\n";
+echo 'They watched <em>' . $movie . '</em> and had a delicious candy, it was ' . $candy . '.';
